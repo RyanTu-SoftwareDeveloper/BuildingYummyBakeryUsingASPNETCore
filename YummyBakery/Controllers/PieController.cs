91e7fac7c88d5edad088e1b1c5f9049869f4bc6a
RyanTu-SoftwareDeveloper <ryantu89.work@gmail.com>
2024-06-06 15:11:28 -0700

    Added the controller

---
+++ YummyBakery/Controllers/PieController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YummyBakery.Models;
+
+namespace YummyBakery.Controllers
+{
+	public class PieController : Controller
+	{
+		private readonly IPieRepository _pieRepository;
+		private readonly ICategoryRepository _categoryRepository;	
+
+		public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+		{
+			_pieRepository = pieRepository;
+			_categoryRepository = categoryRepository;
+		}
+
+		public IActionResult List()
+		{
+			return View(_pieRepository.AllPies);
+		}
+	}
+}
