5743f857336f9b325d9e42cb28b4f87322cc430c
RyanTu-SoftwareDeveloper <ryantu89.work@gmail.com>
2024-06-17 13:32:15 -0700

    Created model and DbSet for shoppingCart

--- YummyBakery/Migrations/20240617202504_AddShoppingCartItem.Designer.cs
+++ YummyBakery/Migrations/20240617202504_AddShoppingCartItem.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using YummyBakery.Models;
 
@@ -10,9 +11,11 @@ using YummyBakery.Models;
 namespace YummyBakery.Migrations
 {
     [DbContext(typeof(YummyBakeryDbContext))]
-    partial class YummyBakeryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240617202504_AddShoppingCartItem")]
+    partial class AddShoppingCartItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
