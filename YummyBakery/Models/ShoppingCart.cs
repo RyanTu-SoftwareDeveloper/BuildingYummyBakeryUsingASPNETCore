ed367acde8253a0007bc6208a659bbc53b13f2d0
RyanTu-SoftwareDeveloper <ryantu89.work@gmail.com>
2024-06-17 16:41:47 -0700

    Created functionality for Shopping Cart

---
+++ YummyBakery/Models/ShoppingCart.cs
@@ -0,0 +1,111 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace YummyBakery.Models
+{
+    public class ShoppingCart : IShoppingCart
+    {
+        private readonly YummyBakeryDbContext _yummyBakeryDbContext;
+
+        public string? ShoppingCartId { get; set; }
+
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; } = default!;
+
+        public ShoppingCart(YummyBakeryDbContext yummyBakeryDbContext)
+        {
+            _yummyBakeryDbContext = yummyBakeryDbContext;
+        }
+
+        public static ShoppingCart GetCart(IServiceProvider services)
+        {
+            ISession? session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext?.Session;
+
+            YummyBakeryDbContext context = services.GetService<YummyBakeryDbContext>() ?? throw new Exception("Error initializing");
+
+            string cartId = session?.GetString("CartId") ?? Guid.NewGuid().ToString();
+
+            session?.SetString("CartId", cartId);
+
+            return new ShoppingCart(context) { ShoppingCartId = cartId };
+        }
+        public void AddToCart(Pie pie)
+        {
+            var shoppingCartItem =
+                     _yummyBakeryDbContext.ShoppingCartItems.SingleOrDefault(
+                        s => s.Pie.PieId == pie.PieId && s.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem == null)
+            {
+                shoppingCartItem = new ShoppingCartItem
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Pie = pie,
+                    Amount = 1
+                };
+
+                _yummyBakeryDbContext.ShoppingCartItems.Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.Amount++;
+            }
+            _yummyBakeryDbContext.SaveChanges();
+        }
+
+        public int RemoveFromCart(Pie pie)
+        {
+            var shoppingCartItem =
+                    _yummyBakeryDbContext.ShoppingCartItems.SingleOrDefault(
+                        s => s.Pie.PieId == pie.PieId && s.ShoppingCartId == ShoppingCartId);
+
+            var localAmount = 0;
+
+            if (shoppingCartItem != null)
+            {
+                if (shoppingCartItem.Amount > 1)
+                {
+                    shoppingCartItem.Amount--;
+                    localAmount = shoppingCartItem.Amount;
+                }
+                else
+                {
+                    _yummyBakeryDbContext.ShoppingCartItems.Remove(shoppingCartItem);
+                }
+            }
+
+            _yummyBakeryDbContext.SaveChanges();
+
+            return localAmount;
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ??=
+                       _yummyBakeryDbContext.ShoppingCartItems.Where(c => c.ShoppingCartId == ShoppingCartId)
+                           .Include(s => s.Pie)
+                           .ToList();
+        }
+
+        public void ClearCart()
+        {
+            var cartItems = _yummyBakeryDbContext
+                .ShoppingCartItems
+                .Where(cart => cart.ShoppingCartId == ShoppingCartId);
+
+            _yummyBakeryDbContext.ShoppingCartItems.RemoveRange(cartItems);
+
+            _yummyBakeryDbContext.SaveChanges();
+        }
+
+
+        public decimal GetShoppingCartTotal()
+        {
+            var total = _yummyBakeryDbContext.ShoppingCartItems.Where(c => c.ShoppingCartId == ShoppingCartId)
+                .Select(c => c.Pie.Price * c.Amount).Sum();
+            return total;
+        }
+
+
+    }
+}
